1f1683873b8712202ab0c16294eb7075b8509a9a
yamada0625shuya <s.yamada@webedge.jp>
2022-04-06 15:15:40 +0900

    4_6
    第４部の問題１のコードを記述しました。

--- Java_problem4/src/ex1/A1.java
+++ Java_problem4/src/ex1/A1.java
@@ -2,7 +2,12 @@ package ex1;
 
 public class A1 {
 	public static void main(String[]args) {
-		int[] num = new int[100];
-
+		int total = 0;
+		for(int i = 1; i <= 100; i++) {
+			if(i%2 == 0) {
+				total = total + i;
+			}
+		}
+		System.out.println(total);
 	}
 }
