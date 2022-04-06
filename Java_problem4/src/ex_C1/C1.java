52a1e01fe91e483bea1276e3d0a828e404404ac5
yamada0625shuya <s.yamada@webedge.jp>
2022-04-06 16:57:08 +0900

    4_6
    問題集の第４部のC1のコードを記述しました。

---
+++ Java_problem4/src/ex_C1/C1.java
@@ -0,0 +1,24 @@
+package ex_C1;
+
+public class C1 {
+	public static void main(String[]args) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (j < 4 - i) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		for(int k = 0; k < 5; k++) {
+			for(int l = 0; l < 5 - k ; l++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
