b6eecf2eb1ff9c77977b6eca815d160f6b50c3b3
yamada0625shuya <s.yamada@webedge.jp>
2022-04-27 09:12:51 +0900

    4_27
    本日のエラー構文を提出いたします。

---
+++ Java_ouyou_problem_all/src/ex5/ex5_main/TraineeDaoMain4.java
@@ -0,0 +1,12 @@
+package ex5.ex5_main;
+
+import java.util.Scanner;
+
+public class TraineeDaoMain4 {
+	public static void main(String[] args) {
+		System.out.println("研修生IDを入力してください -> ");
+		Scanner sc = new Scanner(System.in);
+		int id = sc.nextInt();
+		
+	}
+}
---
+++ error/src/e4_27/Sample13.java
@@ -0,0 +1,44 @@
+package e4_27;
+
+import java.util.Calendar;
+
+public class Sample13 {
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+		String week[] = {"土","日","月","火","水","木","金"};
+
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		String weekday = week[cal.get(Calendar.DAY_OF_WEEK)];
+		System.out.println("今日は" + year + "年" + month + "月" + day + "日" + weekday + "曜日です。");
+	}
+}
+
+
+//エラーコード
+//
+//Exception in thread "main" java.lang.Error: Unresolved compilation problems:
+//	DAY は解決できないか、フィールドではありません
+//	WEEK は解決できないか、フィールドではありません
+//
+//	at e4_27.Sample13.main(Sample13.java:12)
+//
+//
+//
+//エラー内容
+//
+//12行目のDAYとWEEKでエラーが出ています、もしくはフィールドとして扱えません。
+//
+//
+//
+//解決方法
+//
+//何日を表示する場合と何曜日を表示する場合は「DAY_OF_MONTH」と「DAY_OF_WEEK」というように記述する。
+
+
+
+//+α
+//
+//MONTHは最初は「０」から開始されるので「１」を足す。
+
